281383f16e716745fa34960c543ec30af20a9c53
Sebastian A. Rinaldi <sebastian.agustin.rinaldi@gmail.com>
2020-05-20 04:19:26 -0300

    Add comparision between popular words in true and fake tweets

--- functions.py
+++ functions.py
@@ -14,3 +14,34 @@ def getlinks(string):
         return list(map(getDomain, links))
     
     return ();
+
+def limpiar_texto(x):
+    x = re.sub(r"&amp", "and", x)
+    x = re.sub(r"\x89Û_", "", x)
+    x = re.sub(r"\x89ÛÒ", "", x)
+    x = re.sub(r"\x89ÛÓ", "", x)
+    x = re.sub(r"\x89ÛÏ", "", x)
+    x = re.sub(r"let\x89Ûªs", "lets", x)
+    x = re.sub(r"\x89Û÷", "", x)
+    x = re.sub(r"\x89Ûª", "", x)
+    x = re.sub(r"\x89Û\x9d", "", x)
+    x = re.sub(r"å_", "", x)
+    x = re.sub(r"\x89Û¢", "", x)
+    x = re.sub(r"\x89Û¢åÊ", "", x)
+    x = re.sub(r"åÊ", "", x)
+    x = re.sub(r"åÈ", "", x)
+    x = re.sub(r"Ì©", "e", x)
+    x = re.sub(r"å¨", "", x)
+    x = re.sub(r"SuruÌ¤", "suruc", x)
+    x = re.sub(r"åÇ", "", x)
+    x = re.sub(r"åÀ", "", x)
+    x = re.sub(' +', ' ', x) #remove duplicated spaces
+    x=x.lower()
+    x=sacar_puntuacion(x)
+    return x
+
+def sacar_puntuacion(x):
+  puntuacion="@#!?,¿+&*_[]-%.:/();$=><|{}^" + "'`"
+  for p in puntuacion:
+    x=x.replace(p, f'')
+  return x 
